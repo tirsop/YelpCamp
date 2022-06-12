a1252e2ca05fa20bc979d41b5728b8c092866e8f
Tirso Pascual <tpascualgarcia@gmail.com>
2022-06-12 12:15:34 +0900

    routes refactor

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,22 +1,22 @@
 import express from 'express';
 const router = express.Router();
 
-import Campground from '../models/campground.js';           // import mongoose model created inside models folder
 import campgrounds from '../controllers/campgrounds.js';
 
-import catchAsync from '../utils/catchAsync.js';            // try and catch errors in async functions
 import { isLoggedIn, isAuthor, validateCampground } from '../middleware.js';              // what to do if user is not authenticate
+import catchAsync from '../utils/catchAsync.js';            // try and catch errors in async functions
 
+router.route('/')
+  .get(catchAsync(campgrounds.index))
+  .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCamp));
 
+router.get('/new', isLoggedIn, campgrounds.newCamp);
 
+router.route('/:id')
+  .get(catchAsync(campgrounds.showCamp))
+  .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCamp))
+  .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.destroyCamp));
 
-router.get('/', catchAsync(campgrounds.index));         // Index page, list of all items. 
-router.get('/new', isLoggedIn, campgrounds.newCamp);    // Creates new item. 1st display a form (/new), then post to save item (req.body) in the db
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCamp));
-router.get('/:id', catchAsync(campgrounds.showCamp));
-router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.editCamp));          // Updates/edit item. 1st shows pre-filled form, then updates value & redirect to show page.
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCamp));
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.destroyCamp));       // Delete an item by taking its id. 'Delete' button is at the show.ejs
-
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.editCamp));
 
 export default router;
--- routes/users.js
+++ routes/users.js
@@ -1,48 +1,15 @@
 import express from 'express';
 import passport from 'passport';
 const router = express.Router();
-import User from '../models/user.js';
+
+import users from '../controllers/users.js'
+
 import catchAsync from '../utils/catchAsync.js';
-import { promisify } from 'util';
 
-router.get('/register', (req, res) => {
-  res.render('users/register');
-})
-
-router.post('/register', catchAsync(async (req, res, next) => {
-  try {
-    const { email, username, password } = req.body;
-    const user = new User({ email, username });
-    const registeredUser = await User.register(user, password);
-    await promisify(req.login)(registeredUser)
-    req.flash('success', 'Welcome to Yelp Camp!');
-    res.redirect('/campgrounds');
-    // req.login(registeredUser, err => {   // so when user registers, he also logs in
-    //   if (err) return next(err);
-    //   req.flash('success', 'Welcome to Yelp Camp!');
-    //   res.redirect('/campgrounds');
-    // })
-  } catch (e) {
-    req.flash('error', e.message);
-    res.redirect('register');
-  }
-}));
-
-router.get('/login', (req, res) => {
-  res.render('users/login');
-});
-
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), async (req, res) => {
-  req.flash('success', 'Welcome back!');
-  const redirectUrl = req.session.returnTo || '/campgrounds';
-  delete req.session.returnTo;
-  res.redirect(redirectUrl);
-})
-
-router.get('/logout', (req, res) => {
-  req.logout();
-  req.flash('success', 'Goodbye!');
-  res.redirect('/campgrounds');
-})
+router.get('/register', users.renderRegister);
+router.post('/register', catchAsync(users.register));
+router.get('/login', users.renderLogin);
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
+router.get('/logout', users.logout);
 
 export default router;
